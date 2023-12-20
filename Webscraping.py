9f02c82a9df8d9f09201a5f5908ec73323e47b45
KevinPham124 <kpham124@terpmail.umd.edu>
2023-12-19 20:47:55 -0500

    Error message that tells the user that the website they want to scrape from cannot be requested.

--- Webscraping.py
+++ Webscraping.py
@@ -1,8 +1,10 @@
 # INST126 Final Project by Kevin Pham
 # Webscraping Program 
 
-import requests
+#The lines of code below check off the following:
+# - Advanced Topic: Webscraping and JSON: Item 10.2
 from bs4 import BeautifulSoup
+import requests
 
 # This variable allows for a user to copy and paste a website url that they would like to scrape text from
 url = input("What website would you like to webscrape from?: \n")
@@ -19,3 +21,10 @@ if req.status_code == 200:
     soup = BeautifulSoup(req.content, "html.parser")
     # This variable counts the words on the website
     word_count = soup.get_text().lower().split().count(word_to_count.lower())
+    # Message that displays how many words appears in the website
+    print(f"The word '{word_to_count}' appears {word_count} times on the website!")
+
+else: 
+    # Error message to show user that the request was not successful
+    print(f"Uh Oh! We were unable to request the website you wanted. Status code: {req.status_code}")
+
